9953dd637fb48c8cbb747e0c10ec2d1477b0bed2
Dmitry Rozhkov <dmitry.rojkov@gmail.com>
2026-04-21 21:34:14 +0300

    wip

--- exe/gateway/gateway.cc
+++ exe/gateway/gateway.cc
@@ -14,11 +14,15 @@ namespace {
 
 class Application : public carrot::event::IOObject {
 public:
-  void HandleCompletion(int res, uint32_t flags) override {}
+  void HandleCompletion(int res, uint32_t flags) override {
+    assert(dispatcher_ != nullptr);
+    dispatcher_->Shutdown();
+    printf("ooooo\n");
+  }
   void ProcessCommand(carrot::event::Command cmd) override {
     // assert(cmd.type_ == carrot::event::Command::ACTIVATE_READ);
-    auto* dispatcher = static_cast<carrot::event::Dispatcher*>(cmd.args_);
-    struct io_uring* ring = dispatcher->GetRing();
+    dispatcher_ = static_cast<carrot::event::Dispatcher*>(cmd.args_);
+    struct io_uring* ring = dispatcher_->GetRing();
     sigset_t mask;
     sigemptyset(&mask);
     sigaddset(&mask, SIGINT);
@@ -30,13 +34,15 @@ public:
 
     int sfd = signalfd(-1, &mask, 0);
 
-    auto sqe = io_uring_get_sqe(ring);
+    auto* sqe = io_uring_get_sqe(ring);
+    io_uring_sqe_set_data(sqe, this);
 
     io_uring_prep_read(sqe, sfd, &fdsi_, sizeof(fdsi_), 0);
   }
 
 private:
-  struct signalfd_siginfo fdsi_;
+  struct signalfd_siginfo fdsi_{};
+  carrot::event::Dispatcher* dispatcher_{nullptr};
 };
 
 } // namespace
