557884ebfbf6f5bf0d3baed8b35bb8a43d9008b8
Dmitry Rozhkov <dmitry.rojkov@gmail.com>
2026-05-09 18:32:25 +0300

    Move Chunk to its own compilation unit

---
+++ src/core/io/chunk.cc
@@ -0,0 +1,25 @@
+#include "core/io/chunk.hh"
+
+#include <cassert>
+
+namespace carrot::io {
+
+auto Chunk::GetReadArea() -> std::span<std::byte> {
+  return {data_.data() + consumed_, filled_ - consumed_};
+}
+
+auto Chunk::GetWriteArea() -> std::span<std::byte> {
+  return {data_.data() + filled_, BUFFER_SIZE - filled_};
+}
+
+void Chunk::Fill(size_t size) {
+  assert(filled_ + size <= BUFFER_SIZE);
+  filled_ += size;
+}
+
+void Chunk::Consume(size_t size) {
+  assert(ReadableSize() >= size);
+  consumed_ += size;
+}
+
+} // namespace carrot::io
---
+++ src/core/io/chunk.hh
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <cstddef>
+#include <memory>
+#include <span>
+
+namespace carrot::io {
+
+namespace {
+constexpr size_t BUFFER_SIZE{4096};
+}
+
+class Chunk {
+public:
+  auto GetReadArea() -> std::span<std::byte>;
+  auto GetWriteArea() -> std::span<std::byte>;
+  void Fill(size_t size);
+  void Consume(size_t size);
+
+  [[nodiscard]] auto ReadableSize() const -> size_t { return filled_ - consumed_; }
+  [[nodiscard]] auto WritableSize() const -> size_t { return BUFFER_SIZE - filled_; }
+  [[nodiscard]] auto IsFullForReading() const -> bool { return filled_ == BUFFER_SIZE; }
+  [[nodiscard]] auto HasReadableData() const -> bool { return ReadableSize() > 0; }
+
+private:
+  std::array<std::byte, BUFFER_SIZE> data_{};
+  size_t filled_{0};
+  size_t consumed_{0};
+};
+
+using ChunkPtr = std::unique_ptr<Chunk>;
+
+} // namespace carrot::io
